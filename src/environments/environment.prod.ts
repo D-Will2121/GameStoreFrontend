748d276ab007fc3749cd0432f1d4b387bf3d6c41
D-Will2121 <de.will2121@gmail.com>
2022-04-22 15:25:33 -0400

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080'
+  //apiBaseUrl: 'http://localhost:8080'
 
-  //apiBaseUrl: 'https://videogame-store-app.herokuapp.com'
+  apiBaseUrl: 'https://videogame-store-app.herokuapp.com'
 
 };
